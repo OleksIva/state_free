6f14743a183a9662f3f14053a1cfb90d0c5af1eb
Olena Mitropolska <olena.mitropolska@gmail.com>
2024-04-15 15:03:02 +0200

    FE-1: setting up for Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import '../src/styles/App.scss';
 import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Home />
+     <h1>Checking</h1>
     </div>
   );
 }
