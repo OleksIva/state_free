07f7418ab0dfb70d3c1fe4dc1e71de07539e6e73
unknown <meenakumari16@hotmail.com>
2024-04-29 16:56:12 +0200

    FE-5 edit content in the resource view

--- src/pages/Data.tsx
+++
@@ -1,5 +0,0 @@
-export const resourcesData = [
-    { id: 1, title: 'Resource 1', link: 'https://www.wikipedia.org/' },
-    { id: 2, title: 'Resource 2', link: 'https://www.google.com/' },
-    // Add more mocked resources as needed
-  ];
