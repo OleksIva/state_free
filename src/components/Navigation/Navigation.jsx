9c5c82d469d8c59bcc453ef58167dfb4cffd502f
Olena Mitropolska <olena.mitropolska@gmail.com>
2024-07-15 15:27:58 +0200

    FE-13_Navbar_main_design_complete

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,15 +10,17 @@ function Navigation({ user }) {
     return (
         <div>
             <nav>
+            <h2>State Free</h2>
+            
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/ResourcesPage">Resources</Link></li>
+                    {/* <li><Link to="/ResourcesPage">Resources</Link></li> */}
                     <li><Link to="/assignment">Assignment</Link></li>
                     <li><Link to="/faq">FAQ</Link></li>
                     <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">About Us</Link></li>
-                    <li><Link to="/register">Register</Link></li>
+                    <li className='aboutUsButton'><Link to="/about">About Us</Link></li>
                     <li><Link to="/login">Login</Link></li>
+                    <li><Link className='signUpButton' to="/register">Sign Up</Link></li>
                 </ul>
             </nav>
         </div>
