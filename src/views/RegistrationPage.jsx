fd5826a55a16d85de587653f3742c84285a31983
shabanzadeh <shabanzadeh1712@yahoo.com>
2024-08-27 14:28:58 +0200

    update register page

--- src/views/RegistrationPage.jsx
+++ src/views/RegistrationPage.jsx
@@ -1,125 +1,183 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { registerUser } from '../Api/api';
-import { login } from '../store/userSlice';
-import './RegistrationPage.css';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { registerUser } from "../Api/api";
+import { login } from "../store/userSlice";
+import "./RegistrationPage.css";
 
 const RegistrationPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [ort, setOrt] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [ort, setOrt] = useState("");
+  const [error, setError] = useState("");
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
-    setError('');
+    setError("");
   };
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
-    setError('');
+    setError("");
   };
 
   const handleOrtChange = (event) => {
     setOrt(event.target.value);
-    setError('');
+    setError("");
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-    setError('');
+    setError("");
   };
-  
+
   const handleConfirmPasswordChange = (event) => {
     setConfirmPassword(event.target.value);
-    setError('');
+    setError("");
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (password !== confirmPassword) {
-      setError('Passwords do not match. Please try again.');
+      setError("Passwords do not match. Please try again.");
       return;
     }
 
     try {
-      const response = await registerUser(username, email, password, confirmPassword, ort);
+      const response = await registerUser(
+        username,
+        email,
+        password,
+        confirmPassword,
+        ort
+      );
       dispatch(login(response));
-      navigate('/profile');
+      navigate("/profile");
     } catch (error) {
-      const errorMessage = String(error).split('Error: ')[1] || 'Registration failed';
-      console.error('Registration failure:', errorMessage);
-      setError('Failed to register. ' + errorMessage + '. Please try again.');
+      const errorMessage =
+        String(error).split("Error: ")[1] || "Registration failed";
+      console.error("Registration failure:", errorMessage);
+      setError("Failed to register. " + errorMessage + ". Please try again.");
     }
   };
 
   return (
-    <div className="container mt-4 bg-register p-5" style={{ height: '580px', width: '600px' }}>
-      
+    <div
+      className="container mt-5 bg-register p-5 sm-custom-text mobile-container"
+      style={{ height: "550px", width: "670px" }}
+    >
       <div className="h1 fs-5">Enter your information to register</div>
-      
+
       <form onSubmit={handleSubmit}>
         <div className="form-group row mb-3">
-          <label className="col-12 col-md-2 col-form-label">Username</label>
+          <label className="col-12 col-md-2 col-form-label input-label">User name</label>
           <div className="col-12 col-md-10">
-            <input type="text" className="form-control" value={username} onChange={handleUsernameChange} />
+            <input
+              type="text"
+              className="form-control input-field"
+              value={username}
+              onChange={handleUsernameChange}
+            />
           </div>
         </div>
         <div className="form-group row mb-3">
-          <label className="col-12 col-md-2 col-form-label">Email</label>
+          <label className="col-12 col-md-2 col-form-label input-label">Email</label>
           <div className="col-12 col-md-10">
-            <input type="email" className="form-control" value={email} onChange={handleEmailChange} />
+            <input
+              type="email"
+              className="form-control input-field"
+              value={email}
+              onChange={handleEmailChange}
+            />
           </div>
         </div>
         <div className="form-group row mb-3">
-          <label className="col-12 col-md-2 col-form-label">Password</label>
+          <label className="col-12 col-md-2 col-form-label input-label">Password</label>
           <div className="col-12 col-md-10">
-            <input type="password" className="form-control" value={password} onChange={handlePasswordChange} />
+            <input
+              type="password"
+              className="form-control input-field"
+              value={password}
+              onChange={handlePasswordChange}
+            />
           </div>
         </div>
-        <div className="form-group row mb-3">
-          <label className="col-12 col-md-2 col-form-label">Confirm Password</label>
+        <div className="form-group row">
+          <label className="col-12 col-md-2 col-form-label 1h-sm  input-label">
+            Confirm Password
+          </label>
           <div className="col-12 col-md-10">
-            <input type="password" className="form-control" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+            <input
+              type="password"
+              className="form-control input-field"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+            />
           </div>
         </div>
-        <div className="form-group row mb-3">
+        <div className="form-group row mb-3 input-label">
           <label className="col-12 col-md-2 col-form-label">Ort</label>
           <div className="col-12 col-md-10">
-            <input type="text" className="form-control" value={ort} onChange={handleOrtChange} />
+            <input
+              type="text"
+              className="form-control input-field"
+              value={ort}
+              onChange={handleOrtChange}
+            />
           </div>
         </div>
         {error && <div className="alert alert-danger">{error}</div>}
-        
+
         <div className="form-check">
-          <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
-          <label className="form-check-label" htmlFor="defaultCheck1">
-            Please read and accept our data privacy policy.
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="defaultCheck1"
+          />
+          <label className="form-check-label text-checkbox" htmlFor="defaultCheck1">
+            Please read and accept our data{" "}
+            <span className="fw-bold text-decoration-underline">
+              privacy policy.
+            </span>
           </label>
         </div>
-        
+
         <div className="form-check">
-          <input className="form-check-input" type="checkbox" value="" id="defaultCheck2"/>
-          <label className="form-check-label" htmlFor="defaultCheck2">
-            Allow us to save your documents and information based on our data privacy policy.
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="defaultCheck2"
+          />
+          <label className="form-check-label text-checkbox" htmlFor="defaultCheck2">
+            Allow us to save your documents and information based on our data
+            privacy.
           </label>
         </div>
-        <div className="d-flex flex-column align-items-center">
-    <div className="mb-2 w-50">
-      <button type="submit" className="btn btn-success rounded-pill w-100">Registerieren</button>
-    </div>
-    <div className='w-50'>
-      <button type="button" className="btn btn-light rounded-pill w-100 text-dark border-success" onClick={() => navigate('/login')}>
-        Einloggen
-      </button>
-    </div>
-  </div>
-        
+        <div className="d-flex flex-column align-items-center gap-2 sm-custom-text pt-5">
+          <div className="mb-2 w-50">
+            <button
+              type="submit"
+              className="btn-matte-green rounded-pill w-100"
+            >
+              Registerieren
+            </button>
+          </div>
+          <div className="w-50">
+            <button
+              type="button"
+              className="btn btn-light rounded-pill w-100 text-dark border-success"
+              onClick={() => navigate("/login")}
+            >
+              Einloggen
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
