b6c7ab5210a8b8852d3b9fcda20c9de6a54ce7c9
shabanzadeh <shabanzadeh1712@yahoo.com>
2024-09-01 18:25:43 +0200

    login page f

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -30,65 +30,98 @@ const LoginPage = () => {
       dispatch(login(response));
       navigate('/profile');
     } catch (error) {
-      error = String(error).split('Error: ')
+      error = String(error).split('Error: ');
       console.error('Login failure:', error[1]);
       setError('Failed to login. ' + error[1] + '. Please try again.');
     }
   };
 
+  const handleForgotPassword = () => {
+    navigate('/forgot-password');
+  };
+
   return (
     <div className='container login-page'>
-      <div >
-      
-      <div className='row  '>
-        <div className='col-md-12 text-title-login d-flex  justify-content-center'>
-          <p>Melden Sie sich bei Ihrem Konto an</p>
-
-      </div>
-      </div>
-      
-    
-      <form onSubmit={handleSubmit}>
+      <div>
         <div className='row'>
-        <div className='col-md-12 '>
-       
-         
-          <input
-            type="text"
-            value={email}
-            onChange={ handleEmailChange}
-            className='input-email'
-            placeholder='E-Mail'
-            
-          />
-          <i className="bi bi-envelope email-icon"></i>
+          <div className='col-md-12 d-flex justify-content-center'>
+            <p className='text-title-login'>Melden Sie sich bei Ihrem Konto an</p>
+          </div>
         </div>
-        </div>
-        <div className='row'>
-        <div className='col-md-12 '>
-        <div>
-         
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className='input-password'
-            placeholder='Password'
-          />
-        </div>
-        </div>
-        </div>
-        {error && <div style={{ color: 'red' }}>{error}</div>}
-        <button type="submit">Login</button>
-      </form>
-      </div>
-      </div>
       
-    
+        <form onSubmit={handleSubmit}>
+          <div className='row'>
+            <div className='col-md-12'>
+              <input
+                type="text"
+                value={email}
+                onChange={handleEmailChange}
+                className='input-email'
+                placeholder='E-Mail'
+              />
+              <i className="bi bi-envelope email-icon"></i>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-md-12'>
+              <input
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                className='input-password'
+                placeholder='Password'
+              />
+            </div>
+          </div>
+          <div className='row'>
+            <div className="form-check col-6">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="defaultCheck1"
+              />
+              <label className="text-checkbox" htmlFor="defaultCheck1">
+                Eingeloggt bleiben
+              </label>
+            </div>
+            <div className="col-6">
+              <p className="forgot-password" onClick={handleForgotPassword}>Passwort vergessen?</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <div className="w-50 col-12">
+                {error && <div style={{ color: 'red' }}>{error}</div>}
+                <button
+                  type="submit"
+                  className="btn-home-einlogen-login rounded-pill"
+                >
+                  Einloggen
+                </button>
+              </div>
+              <div className='row'>
+                <div className='col-12 d-flex justify-content-center'>
+                  <div className='text-register-loinpage'>
+                    haben Sie noch kein Konto?
+                  </div>
+                </div>
+                <div className="col-12 d-flex justify-content-center">
+                  <button
+                    type="button"
+                    className="rounded-pill btn-home-register-loinpage"
+                    onClick={() => navigate("/register")}
+                  >
+                    Registerieren
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
 export default LoginPage;
-
-
-
