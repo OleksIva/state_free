f490b7723b10d78021df491d63dbb77ebc65062d
Oleksandr Ivanhorodskyi <sashaivaa7@gmail.com>
2024-09-29 08:54:02 +0200

    FAQ fixed

--- src/views/FAQPage.jsx
+++ src/views/FAQPage.jsx
@@ -1,27 +1,30 @@
-import React, { useState } from "react";
-import FaqsButton from "../components/Faqs/FaqsButton";
-import StaatenlosigkeitButton from "../components/Faqs/StaatenlosigkeitButton";
+import React from "react";
 import FAQList from "../components/Faqs/FAQList";
 import StaatenlosigkeitList from "../components/Faqs/StaatenlosigkeitList";
 import "./Faq.css";
-import "./Staatenlosigkeit.css"
+import "./Staatenlosigkeit.css";
 
 const FAQPage = () => {
-  const [showFAQs, setShowFAQs] = useState(false);
-  const [showStaatenlosigkeit, setShowStaatenlosigkeit] = useState(false);
-  
   return (
     <div className="faq-page">
-       <h1>faq</h1>
-      <FaqsButton onClick={() => setShowFAQs(prev =>  !prev)} />
-      <div style={{ border: '1px solid red' }}>
-        <FAQList isVisible={showFAQs} />
+      <h1>FAQ</h1>
+      <h2>Fragen zum Konto</h2>
+      <div>
+        <FAQList isVisible={true} />
       </div>
-      <StaatenlosigkeitButton onClick={() => setShowStaatenlosigkeit(prev =>  !prev)} />
-      <div style={{ border: '1px solid red' }}>
-        <StaatenlosigkeitList isVisible={showStaatenlosigkeit} />
+      
+      <h2>Ungeklärte Staatsangehörigkeit und Staatenlosigkeit</h2>
+      <div>
+        <StaatenlosigkeitList isVisible={true} />
       </div>
     </div>
   );
 };
+
 export default FAQPage;
+
+
+
+
+
+
