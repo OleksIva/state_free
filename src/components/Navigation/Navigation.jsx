1ceac0f1596b8553769c10bd80e23c32d142179a
unknown <meenakumari16@hotmail.com>
2024-10-14 10:19:40 +0200

    add logout button

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,42 +1,19 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom';
-// import './navigation.css';
-
-// function Navigation({ user }) {
-//     if (!user) {
-//         return null
-//     }
-
-//     return (
-//         <div className='pg-0'>
-//             <nav>
-//                 <img src="https://ik.imagekit.io/lnmtrplsk/image%201.png?updatedAt=1721816809003" alt="logo" />
-
-//                 <ul>
-//                     <li><Link to="/">Startseite</Link></li>
-//                     <li><Link to="/faqpage">FAQ</Link></li>
-//                     <li><Link to="/about">Über uns</Link></li>
-//                     <li><Link className='anmeldenButton' to="/login">Anmelden</Link></li>
-//                     <li><Link className='registrierenButton' to="/register">Registrieren</Link></li>
-//                 </ul>
-//             </nav>
-//         </div>
-//     )
-// }
-
-// export default Navigation
-
-
-
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../store/userSlice';
 import './navigation.css';
-import { useSelector } from 'react-redux';
 
 function Navigation() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const loggedIn = useSelector((state) => state.user.userInfo.loggedIn);
-  
+  const handleLogout = () => {
+    dispatch(logout());
+    localStorage.removeItem('user');
+    navigate('/');
+  };
+
   return (
     <div className="pg-0">
       <nav>
@@ -46,7 +23,12 @@ function Navigation() {
           <li><Link to="/faqpage">FAQ</Link></li>
           <li><Link to="/about">Über uns</Link></li>
           {loggedIn ? (
+            <li>
+              <button className="btn btn-logout" onClick={handleLogout}>
+                Logout
+              </button>
               <li><Link to="/assessment/step1">Assessment</Link></li>
+            </li>
           ) : (
             <>
               <li><Link className="anmeldenButton" to="/login">Anmelden</Link></li>
